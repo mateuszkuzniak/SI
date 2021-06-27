92be2914612d12719d9da489373034141eeb1761
bart-kosmala <bartoshmen@gmail.com>
2021-06-27 11:49:43 +0200

    requirements cleanup

--- src/components.py
+++ src/components.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from typing import Generator, List, Optional, Tuple, Callable, Union
+from typing import Generator, List, Optional, Tuple, Callable
 
 from src.util import SolverResults
 
